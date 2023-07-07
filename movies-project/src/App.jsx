ca9fe2ea505373b6ac2f1aca9994efbd6fdfd32a
YildizDikme <103577317+YildizDikme@users.noreply.github.com>
2023-07-07 03:07:41 +0300

    TMDB Api

---
+++ movies-project/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import PageNotFound from "./pages/404/PageNotFound";
+
+function App() {
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
+
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
+
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
+
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
+
+            dispatch(getApiConfiguration(url));
+        });
+    };
+
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
